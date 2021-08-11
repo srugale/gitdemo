ed60ee8d2d551996056c8125fe9242c9dd108cf1
santosh <srugale@gmail.com>
2021-08-11 16:42:57 +0530

    first commit

---
+++ src/alpha/Demo.java
@@ -0,0 +1,18 @@
+package alpha;
+import org.apache.logging.log4j.*;
+
+public class Demo {
+	
+	private static Logger log = LogManager.getLogger(Demo.class.getName()); //common syntax for Log4j
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		log.debug("I have clicked on button");
+		log.info("Button is displayed");
+		log.error("Button is not displayed");
+		log.fatal("Button is missing");
+
+	}
+
+}
---
+++ src/beta/Demo1.java
@@ -0,0 +1,18 @@
+package beta;
+import org.apache.logging.log4j.*;
+
+public class Demo1 {
+	
+	private static Logger log = LogManager.getLogger(Demo1.class.getName()); //common syntax for Log4j
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		log.debug("I have clicked on button");
+		log.info("Button is displayed");
+		log.error("Button is not displayed");
+		log.fatal("Button is missing");
+
+	}
+
+}
